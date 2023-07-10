3b858bb8ec5751e313e730cf3732a0a7ee82d0c9
azizbek3767 <106603988+azizbek3767@users.noreply.github.com>
2023-07-10 18:43:37 +0500

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,332 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <title>FS Advisory</title>
+
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-574-mexant.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
+  </head>
+
+<body>
+  <header class="header-area header-sticky">
+      <div class="container">
+          <div class="row">
+              <div class="col-12">
+                  <nav class="main-nav">
+                      <a href="index.html" class="logo">
+                          <img src="assets/images/logo.png" alt="">
+                      </a>
+                      <ul class="nav">
+                          <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                          <li class="scroll-to-section"><a href="#about-us">About Us</a></li>
+                          <li class="scroll-to-section"><a href="#contact-us">Contact Us</a></li>
+                      </ul>        
+                      <a class='menu-trigger'>
+                          <span>Menu</span>
+                      </a>
+                  </nav>
+              </div>
+          </div>
+      </div>
+  </header>
+
+  <div class="swiper-container" id="top">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">
+        <div class="slide-inner" style="background-image:url(assets/images/slide-01.jpg)">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8">
+                <div class="header-text">
+                       
+                  <h2>EXPERTS WITH A <em>PASSION</em> FOR<br> CHANGE <em>THAT MATTERS</em></h2>
+                  <div class="div-dec"></div>
+                  <p>This is what we want to
+                    be but also why we hope you come to us. We believe that people who continue to develop
+                    themselves are best equipped to really contribute and make a sustainable difference in an
+                    ever-changing world in which there is also a growing urgency to create more equitable
+                    societies.</p>
+                  <div class="buttons">
+                    <div class="green-button">
+                      <a href="#contact-us">Contact Us</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="swiper-slide">
+        <div class="slide-inner" style="background-image:url(assets/images/slide-02.jpg)">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8">
+                <div class="header-text">
+                  <h2><em>GLOBAL </em> PARTNERS WITH <br> <em>LOCAL </em> EXPERTISE</h2>
+                  <div class="div-dec"></div>
+                  <p>With support from our global partners,
+                    clients are able to tap into our worldwide network and call upon combined teams of
+                    international and local experts of FS Advisory for training and consultancy assignments.
+                    Our participants in training courses organized all over the world also enjoy meeting with
+                    and learning from fellow professionals with diverse cultural backgrounds.</p>
+                  <div class="buttons">
+                    <div class="green-button">
+                      <a href="#contact-us">Contact Us</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="swiper-slide">
+        <div class="slide-inner" style="background-image:url(assets/images/slide-03.jpg)">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8">
+                <div class="header-text">
+                  <h2>MASTERS<br>&amp; IN BRIDGING <em>THEORY AND PRACTICE</em></h2>
+                  <div class="div-dec"></div>
+                  <p>We have hands-on experience
+                    working on a range of different assignments at local level. We have become masters in
+                    bridging theory and practice and apply these insights in the training courses we deliver.</p>
+                  <div class="buttons">
+                    <div class="green-button">
+                      <a href="#contact-us">Contact Us</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+    <div class="swiper-button-next swiper-button-white"></div>
+    <div class="swiper-button-prev swiper-button-white"></div>
+  </div>
+
+  <section class="about-us" id="about-us">
+    <section class="top-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 offset-lg-3">
+            <div class="section-heading">
+              <h6>About Us</h6>
+              <h4>Know Us Better</h4>
+            </div>
+          </div>
+          <div class="col-lg-12 align-self-center" style="margin-bottom: 40px;">
+            <div class="accordions is-first-expanded">
+              <h3 class="mb-4">Company overview</h3>
+              <p>FS Advisory is a consulting firm in Uzbekistan which provides consulting, training and advisory services. We enable projects & programs, professionals, organizations, partnerships & networks to achieve sustainable results and results that matter for a better world. We do this because we are convinced that people who continue to develop themselves are best equipped to make a sustainable difference towards more equitable societies. In addition to our trainings for private clients, we also have experience with the projects financed by International Financial Institutions such as World Bank.
+                With this spirit and focus we warmly invite you to call upon our services, including open-entry and tailor-made training courses, advisory, and coaching. Wherever, and whenever, we can add value to your activities.</p>
+            </div>
+          </div>
+          <div class="col-lg-12">
+            <div class="service-item">
+              <div class="row">
+                <div class="col-lg-6 align-self-center">
+                  <div class="right-text-content">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="left-image">
+              <img src="assets/images/about-left-image.jpg" alt="">
+            </div>
+          </div>
+          <div class="col-lg-6 align-self-center">
+            <div class="accordions is-first-expanded">
+              <article class="accordion">
+                <div class="accordion-head">
+                    <span>Sector focus</span>
+                    <span class="icon">
+                        <i class="icon fa fa-chevron-right"></i>
+                    </span>
+                </div>
+                <div class="accordion-body">
+                    <div class="content">
+                        <p>FS Advisory offers services to its clients in water supply and sanitation, sewerage, retail
+                          and financial services sectors. 
+                        <br><br>We continuously grow our team and try expand our services
+                        to other sectors as well.</p>
+                    </div>
+                </div>
+            </article>
+            <article class="accordion">
+              <div class="accordion-head">
+                  <span>Team</span>
+                  <span class="icon">
+                      <i class="icon fa fa-chevron-right"></i>
+                  </span>
+              </div>
+              <div class="accordion-body">
+                  <div class="content">
+                      <p>We have the mixture of full-time and short-term (intermittent) trainers who experts in their
+                        respective field. We also cooperate with our international partners in organizing training
+                        and seminars. 
+                      <br><br>All experts operate both as trainer and consultant, enabling them to enrich
+                      training courses with their own practice from the field, whilst applying the latest
+                      management concepts in their consultancy work.</p>
+                  </div>
+              </div>
+            </article>
+            <article class="accordion">
+              <div class="accordion-head">
+                  <span>Project references</span>
+                  <span class="icon">
+                      <i class="icon fa fa-chevron-right"></i>
+                  </span>
+              </div>
+              <div class="accordion-body">
+                  <div class="content">
+                      <p>Assignment name: Capacity development/training for the employees of Mega Venture
+                        Topics: Marketing and sales strategy, Development of business plans and strategies,
+                        Budgeting, Procurement and contract management.</p>
+                  </div>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section> 
+  </section>
+      </div>
+    </div>
+  </section> 
+
+ 
+
+  <section class="map" id="contact-us">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 offset-lg-3">
+          <div class="section-heading">
+            <!-- <h6>Contact us</h6> -->
+            <h4>Contact us</h4>
+          </div>
+        </div>
+        <div class="col-lg-12">
+          <div id="map">
+            <iframe src="https://maps.google.com/maps?q=32A, Amir Temur Avenue, Yunusobod district, Tashkent, Uzbekistan&t=&z=15&ie=UTF8&iwloc=&output=embed" width="100%" height="450px" frameborder="0" style="border:0; border-radius: 5px; position: relative; z-index: 2;" allowfullscreen=""></iframe>
+          </div>
+        </div>
+        <div class="col-lg-10 offset-lg-1">
+          <div class="row">
+            <div class="col-lg-4">
+              <div class="info-item">
+                <i class="fa fa-envelope"></i>
+                <h4>Email Address</h4>
+                <a href="mailto:info@fsadvisory.uz">info@fsadvisory.uz</a>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="info-item">
+                <i class="fa fa-phone"></i>
+                <h4>Phone Number</h4>
+                <a href="tel:+998909952725">+998 90 995 2725</a>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="info-item">
+                <i class="fa fa-map-marked-alt"></i>
+                <h4>Address</h4>
+                <a href="https://www.google.com/maps/place/32a+%D0%BF%D1%80%D0%BE%D1%81%D0%BF%D0%B5%D0%BA%D1%82+%D0%90%D0%BC%D0%B8%D1%80%D0%B0+%D0%A2%D0%B5%D0%BC%D1%83%D1%80%D0%B0,+%D0%A2%D0%B0%D1%88%D0%BA%D0%B5%D0%BD%D1%82+100037,+%D0%A3%D0%B7%D0%B1%D0%B5%D0%BA%D0%B8%D1%81%D1%82%D0%B0%D0%BD/@41.3710544,69.2864303,17z/data=!3m1!4b1!4m6!3m5!1s0x38aef334a0e55555:0xa1d8fff833ee3a3b!8m2!3d41.3710504!4d69.2890106!16s%2Fg%2F11kbm54j72">32A, Amir Temur Avenue, Yunusobod district, Tashkent, Uzbekistan</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <p>FS Advisory</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="assets/js/isotope.min.js"></script>
+    <script src="assets/js/owl-carousel.js"></script>
+
+    <script src="assets/js/tabs.js"></script>
+    <script src="assets/js/swiper.js"></script>
+    <script src="assets/js/custom.js"></script>
+    <script>
+        var interleaveOffset = 0.5;
+
+      var swiperOptions = {
+        loop: true,
+        speed: 1000,
+        grabCursor: true,
+        watchSlidesProgress: true,
+        mousewheelControl: true,
+        keyboardControl: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        },
+        on: {
+          progress: function() {
+            var swiper = this;
+            for (var i = 0; i < swiper.slides.length; i++) {
+              var slideProgress = swiper.slides[i].progress;
+              var innerOffset = swiper.width * interleaveOffset;
+              var innerTranslate = slideProgress * innerOffset;
+              swiper.slides[i].querySelector(".slide-inner").style.transform =
+                "translate3d(" + innerTranslate + "px, 0, 0)";
+            }      
+          },
+          touchStart: function() {
+            var swiper = this;
+            for (var i = 0; i < swiper.slides.length; i++) {
+              swiper.slides[i].style.transition = "";
+            }
+          },
+          setTransition: function(speed) {
+            var swiper = this;
+            for (var i = 0; i < swiper.slides.length; i++) {
+              swiper.slides[i].style.transition = speed + "ms";
+              swiper.slides[i].querySelector(".slide-inner").style.transition =
+                speed + "ms";
+            }
+          }
+        }
+      };
+
+      var swiper = new Swiper(".swiper-container", swiperOptions);
+    </script>
+  </body>
+</html>
+    </>
   );
 }
 
